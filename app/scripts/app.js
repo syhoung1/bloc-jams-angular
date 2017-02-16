e21ab9692be95d2a30149740e858565b11407abf
Steven <syhoung1@gmail.com>
2017-02-16 07:54:25 +0000

    checkpoint3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-angular.module('blocJams', []);
+(function () {
+    function config($stateprovider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateprovider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+    }
+    
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+    
+})();
