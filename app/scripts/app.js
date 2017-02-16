0f1054e97aed5ec1e9c809c8f62a3d2c9377fad1
Steven <syhoung1@gmail.com>
2017-02-16 08:09:53 +0000

    angular assignment 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,11 @@
                 url: '/album',
                 templateUrl: '/templates/album.html'
             })
+            
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
     }
     
     angular
