ffbf1cbffa225ea7a672bf2028d49878014a7424
Steven <syhoung1@gmail.com>
2017-02-24 19:31:12 +0000

    added eslint to modules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@
             })
             
             .state('collection', {
-                url: '/collection',
+                url: 'collection',
                 controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
